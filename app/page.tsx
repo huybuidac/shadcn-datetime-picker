a72025360e9106c4f57b221f061b135747d1352d
huy bui dac <huybuidac@gmail.com>
2024-10-07 01:06:32 +0700

    feat: link to github

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,9 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <div className="flex-1 m-20 flex flex-col gap-10 w-[270px]">
+          <a href="https://github.com/huybuidac/shadcn-datetime-picker" target="_blank" rel="noopener noreferrer" className="text-blue-500 underline">
+            View on GitHub
+          </a>
           <h2 className="text-xl font-bold">Datetime Picker</h2>
           <DateTimePicker value={date} onChange={setDate} />
           <h2 className="text-xl font-bold mt-10">Datetime Picker with timezone = UTC</h2>
